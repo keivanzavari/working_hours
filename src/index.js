abd7c1c21d742b07080f8a851f1269a49d3c4a22
Keivan Zavari <keivan.zavari@gmail.com>
2018-02-19 08:44:33 +0100

    added a new component for testing and playing with daypicker module

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TestApp from './TestApp';
+import App from './App';
+import DayPickerTest from './DayPickerTest';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<TestApp />,
+ReactDOM.render(<DayPickerTest />,
 	document.getElementById('root'),
 );
 
