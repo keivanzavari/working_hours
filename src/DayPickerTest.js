92cb088565615314942590e599f1792b95cdb8ae
Keivan Zavari <keivan.zavari@gmail.com>
2018-02-27 07:38:33 +0100

    more complex modifier for react-day-picker

--- src/DayPickerTest.js
+++ src/DayPickerTest.js
@@ -3,6 +3,27 @@ import DayPicker from 'react-day-picker';
 
 import 'react-day-picker/lib/style.css';
 
+const customStyle = `.DayPicker-Day--highlighted {
+  background-color: orange;
+  color: white;
+}
+.DayPicker-Day--exact {
+  background-color: orange;
+  color: white;
+}`;
+
+
+// highlighted:
+// from: new Date(2018, 1, 1),
+// to: new Date(2018, 1, 12),
+const highlighted = {
+  highlighted: {
+    from: new Date(2018, 1, 15),
+    to: new Date(2018, 1, 19),
+  },
+  exact: [new Date(2018, 1, 1), new Date(2018, 1, 2)],
+};
+
 export default class DayPickerTest extends React.Component {
   constructor(props) {
     super(props);
@@ -15,9 +36,12 @@ export default class DayPickerTest extends React.Component {
     this.setState({ selectedDay: day });
   }
   render() {
+
     return (
       <div>
+        <style>{customStyle}</style>
         <DayPicker
+          modifiers={ highlighted }
           onDayClick={this.handleDayClick}
           selectedDays={this.state.selectedDay}
         />
