f94aac3242065c58fb894e9c06a558b34a1e20d2
Keivan Zavari <keivan.zavari@gmail.com>
2018-02-27 07:38:58 +0100

    Use member handler as a new class

--- src/MemberHandler.js
+++ src/MemberHandler.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import members from './members.js';
 
-function MemberHandler(props) {
-  return (
-      <div>
-      <input type="radio" name="gender" value={members[props.index]}/>
-      {members[props.index]}
-      </div>
-  );
-}
+export default class MemberHandler {
+  // <div>
+  // <input type="radio" name="gender" value={members[props.index]}/>
+  // {members[props.index]}
+  // </div>
 
-export default MemberHandler;
+  constructor() {
+    this.id = "0";
+    this.name = "";
+  }
+
+  isMember(id) {
+    // the only search method is indexOf from javascript
+    // if the dataset grows this would need to be improved
+    const idx = members["id"].indexOf(id);
+    if (idx === -1) {
+      return false;
+    } else {
+      this.name = members["name"][idx];
+      return true;
+    }
+
+  }
+}
