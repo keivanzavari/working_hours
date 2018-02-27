fdca904e13a3faaab6e2bea8618ebee27f37ef5b
Keivan Zavari <keivan.zavari@gmail.com>
2018-02-27 07:39:18 +0100

    changed to render App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TestApp from './TestApp';
+// import TestApp from './TestApp';
 import App from './App';
-import DayPickerTest from './DayPickerTest';
+// import DayPickerTest from './DayPickerTest';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<DayPickerTest />,
+ReactDOM.render(<App />,
 	document.getElementById('root'),
 );
 
